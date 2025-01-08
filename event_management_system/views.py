253664ca271a75a36fa7de15dab5f767420efe18
Abubakar Ibrhim Umar <abubakaribrahim30012@gmail.com>
2025-01-08 15:14:36 +0100

    minor changes

--- event_management_system/views.py
+++ event_management_system/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from events.models import Event, EventRegistration
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def home(request):
-    return render(request, 'index.html', {})
+    users = User.objects.count()
+    events = Event.objects.count()
+    
+    context = {
+        'users': users,
+        'events': events,
+    }
+    return render(request, 'index.html', context)
